9da72de21bee8b2f236bdef3d2c9d6b2be3be16f
Tahir <tahirmama8@gmail.com>
2021-11-07 13:21:27 +0500

    Tv App
    Searching data from Api and showing it onm screen

---
+++ src/component/TV.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+import "../App.css";
+import axios from 'axios';
+
+export default function TV() {
+    const [loading, setLoading] = useState(false);
+    const [post, setPosts] = useState([]);
+    const [searchTitle, setsearchTitle] = useState("");
+
+    useEffect(() => {
+        const loadPosts = async () => {
+            setLoading(true);
+            const response = await axios.get("https://shopping-google-scrapper.herokuapp.com/api/search?k=tv");
+            setPosts(response.data);
+            setLoading(false)
+        };
+        loadPosts();
+    }, []);
+
+    return (
+        <div className="App">
+            <br/>
+            <h1 align="center">TV</h1>
+            <br/>
+            <input
+                type="text"
+                placeholder="What are you looking for ..."
+                onChange={(e) => setsearchTitle(e.target.value)}
+            className="Search"
+            />
+            <br/>
+            <br/>
+            {loading ? (
+                <h4>Loading ....</h4>
+            ) : (
+                post.filter((value) => {
+                    if (searchTitle === "") {
+                        return value;
+                    } else if 
+                    (value.title.toLowerCase().includes(searchTitle.toLowerCase()))
+                     {
+                        return value;
+                    }else if 
+                    (value.prices.includes(searchTitle))
+                     {
+                        return value;
+                    }
+                })
+                .map((item) => <div key={item.id} className="innerContainer">
+                    <a href={item.link} target="blank">
+                            <img src={item.image} alt={item.title } className="Pics" />
+                            </a>
+                            <br/>
+                            <br/>
+                    <strong>Title:</strong> {item.title}
+                <br/>   
+                <strong>Prices:</strong> {item.prices}
+                </div>)
+               
+                
+
+            )}
+        </div>
+    );
+}
